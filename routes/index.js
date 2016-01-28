36bdd895cb873d89d9dd06a23e0c16bf77b8d69a
James Bonnett <bonnett89@gmail.com>
2016-01-28 15:35:37 +0000

    [TASK] Add all files and directories from initial experiment

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello World!' });
+});
+
+router.get('/lightlevel', function(req, res) {
+  var db = req.db;
+  var collection = db.get('lightlevel');
+  collection.find({},{},function(e, docs) {
+    res.render('lightlevels', {
+      "lightlevels" : docs
+    });
+  });
+});
+
+module.exports = router;
