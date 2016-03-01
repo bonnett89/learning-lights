f6a6c74be987113f38b51801cac8d9e57096bbae
James Bonnett <bonnett89@gmail.com>
2016-03-01 13:36:48 +0000

    [TASK] Add navbar component, actions and store

---
+++ app/components/Navbar.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Link} from 'react-router';
+import NavbarStore from '../stores/NavbarStore';
+import NavbarActions from '../actions/NavbarActions';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = NavbarStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    NavbarStore.listen(this.onChange);
+
+    $(document).ajaxStart(() => {
+      NavbarActions.updateAjaxAnimation('fadeIn');
+    });
+
+    $(document).ajaxComplete(() => {
+      setTimeout(() => {
+        NavbarActions.updateAjaxAnimation('fadeOut');
+      }, 750);
+    });
+  }
+
+  componentWillUnmount() {
+    NavbarStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <nav className='navbar navbar-default navbar-static-top'>
+        <div className='navbar-header'>
+          <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'>
+            <span className='sr-only'>Toggle navigation</span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+          </button>
+          <Link to='/' className='navbar-brand'>
+            <span ref='triangles' className={'triangles animated ' + this.state.ajaxAnimationClass}>
+              <div className='tri invert'></div>
+              <div className='tri invert'></div>
+              <div className='tri'></div>
+              <div className='tri invert'></div>
+              <div className='tri invert'></div>
+              <div className='tri'></div>
+              <div className='tri invert'></div>
+              <div className='tri'></div>
+              <div className='tri invert'></div>
+            </span>
+            Learning Light System
+            <span className='badge badge-up badge-danger'>{this.state.onlineUsers}</span>
+          </Link>
+        </div>
+        <div id='navbar' className='navbar-collapse collapse'>
+          <ul className='nav navbar-nav'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/lightlevel'>Light Level</Link></li>
+            <li><Link to='/light'>Lights</Link></li>
+            <li><Link to='/lightingmode'>Lighting Mode</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
---
+++ app/stores/NavbarStore.js
@@ -0,0 +1,21 @@
+import alt from '../alt';
+import NavbarActions from '../actions/NavbarActions';
+
+class NavbarStore {
+  constructor() {
+    this.bindActions(NavbarActions);
+    this.onlineUsers = 0;
+    this.searchQuery = '';
+    this.ajaxAnimationClass = '';
+  }
+
+  onUpdateOnlineUsers(data) {
+    this.onlineUsers = data.onlineUsers;
+  }
+
+  onUpdateAjaxAnimation(className) {
+    this.ajaxAnimationClass = className; //fadein or fadeout
+  }
+}
+
+export default alt.createStore(NavbarStore);
