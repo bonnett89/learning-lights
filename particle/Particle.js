a257167aebd4ffe03b60f6b2d1a1c3821d9c95cd
James Bonnett <bonnett89@gmail.com>
2016-03-03 17:46:29 +0000

    [TASK] Create particle file which contains methods for interacting with spark core

---
+++ particle/Particle.js
@@ -0,0 +1,62 @@
+var Particle = require('particle-api-js');
+
+var deviceId = '53ff71066667574827382467';
+var token = '8949c6593b9a5f289e0c9b632270c4b29cd97cd1';
+
+function logIn (user, pass, callback) {
+  var particle = new Particle();
+  particle.login({username: user, password: pass}).then(
+    function(data){
+      callback(null, data);
+    },
+    function(err){
+      console.log('An error occured while logging in: ' + err);
+    }
+  );
+}
+
+function getLightLevel (callback) {
+  var particle = new Particle();
+  particle.getVariable({ deviceId: deviceId, name: 'lightReading', auth: token }).then(
+    function(data){
+      var value = data.body['result'];
+      callback(null, value);
+    }, 
+    function(err) {
+        console.log('An error occurred while getting attrs:', err);
+        callback(err);
+    }
+  );
+}
+
+function lightOn () {
+  var particle = new Particle();
+  var fnPr = particle.callFunction({ deviceId: '53ff71066667574827382467', name: 'light', argument: 'on', auth: '4a5d5ba88a276fc988ad123247d9aeff744626c5' });
+  
+  fnPr.then(
+    function(data) {
+      console.log('Function called succesfully:', data);
+    }, 
+    function(err) {
+      console.log('Function: An error occurred:', err);
+    }
+  );
+}
+
+function lightOff () {
+  var particle = new Particle();
+  var fnPr = particle.callFunction({ deviceId: '53ff71066667574827382467', name: 'light', argument: 'off', auth: '4a5d5ba88a276fc988ad123247d9aeff744626c5' });
+              fnPr.then(
+                function(data) {
+                    console.log('Function called succesfully:', data);
+                }, function(err) {
+                    console.log('Function: An error occurred:', err);
+            });
+}
+
+module.exports = {
+  logIn: logIn,
+  getLightLevel: getLightLevel,
+  lightOn: lightOn,
+  lightOff: lightOff 
+};
