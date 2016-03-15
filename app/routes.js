3dbcef36eca012f93f3775993db14315bab17d9b
James Bonnett <bonnett89@gmail.com>
2016-03-15 12:13:45 +0000

    [TASK] Update routes and navbar component

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,12 @@ import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import LightLevels from './components/LightLevels';
+import LogLightData from './components/LogLightData';
 
 export default (
   <Route component={App}>
     <Route path='/' component={Home} />
     <Route path='/lightlevel' component={LightLevels} />
+    <Route path='/loglightdata' component={LogLightData} />
   </Route>
 );
